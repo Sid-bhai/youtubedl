6c1950cbb235536c168083e02bebb900475e7ad3
Sid Bhai <150309376+Sid-bhai@users.noreply.github.com>
2024-08-18 11:19:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,50 @@ import youtube from './endpoint.js';
 
 const app = express();
 
+app.get('/youtube/quality', async (req, res) => {
+  const { url, quality } = req.query;
+  
+  if (!url || !quality) {
+    return res.status(400).json({
+      creator: 'Ethix-Xsid',
+      status: 400,
+      success: false,
+      message: 'URL or quality parameter is missing',
+    });
+  }
+  
+  try {
+    const result = await youtube(url, quality, false);
+    if (result.status !== 200) {
+      return res.status(result.status).json({
+        creator: 'Ethix-Xsid',
+        status: result.status,
+        success: false,
+        message: 'Failed to process YouTube URL',
+        error: result.error || 'Unknown error',
+      });
+    }
+    
+    res.status(200).json({
+      creator: 'Ethix-Xsid',
+      status: 200,
+      success: true,
+      title: result.title,
+      type: 'video',
+      quality: result.quality,
+      url: result.url,
+    });
+  } catch (error) {
+    res.status(500).json({
+      creator: 'Ethix-Xsid',
+      status: 500,
+      success: false,
+      message: 'An error occurred while processing the YouTube URL',
+      error: error.message,
+    });
+  }
+});
+
 app.get('/youtube/mp4', async (req, res) => {
   const { url } = req.query;
   if (!url) {
@@ -28,14 +72,9 @@ app.get('/youtube/mp4', async (req, res) => {
       creator: 'Ethix-Xsid',
       status: 200,
       success: true,
-      url: result.url,
       title: result.title,
-      author: result.author,
-      views: result.views,
-      lengthSeconds: result.lengthSeconds,
-      size: result.size,
-      publishDate: result.publishDate,
-      timeSincePublished: result.timeSincePublished,
+      type: 'video',
+      url: result.url,
     });
   } catch (error) {
     res.status(500).json({
@@ -73,14 +112,9 @@ app.get('/youtube/mp3', async (req, res) => {
       creator: 'Ethix-Xsid',
       status: 200,
       success: true,
-      url: result.url,
       title: result.title,
-      author: result.author,
-      views: result.views,
-      lengthSeconds: result.lengthSeconds,
-      size: result.size,
-      publishDate: result.publishDate,
-      timeSincePublished: result.timeSincePublished,
+      type: 'audio',
+      url: result.url,
     });
   } catch (error) {
     res.status(500).json({
